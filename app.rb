867735f6c1cc0234b3afe557840d40808b024de9
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-06-08 10:52:21 -0400

    Pass tests

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  post "/checkout" do
+    params[:item]
+    session[:item] = params[:item]
+  end
 end
